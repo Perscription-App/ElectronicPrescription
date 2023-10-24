11bdf79fce2450c83663aeba09b310a21310394c
Erik Ly <erikly11@gmail.com>
2023-10-24 15:36:01 -0500

    Update PrescriptionManagementSystemApplication.java

--- API/src/main/java/com/RWWR/PrescriptionManagementSystemApplication.java
+++ API/src/main/java/com/RWWR/PrescriptionManagementSystemApplication.java
@@ -2,8 +2,10 @@ package com.RWWR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:secret.env")
 public class PrescriptionManagementSystemApplication {
 
 	public static void main(String[] args) {
