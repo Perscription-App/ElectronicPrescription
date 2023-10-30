a3dae25593297a7dd9579981c6dedacb7f120be9
Erik Ly <erikly11@gmail.com>
2023-10-30 15:23:12 -0500

    added sign in

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
-    return (
-      <div>
-        <h1>Welcome to our App!</h1>
-        <p>Please sign in to continue.</p>
-        <button onClick={handleSignIn}>Sign In</button>
-      </div>
-    );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    function handleSignIn() {
-      alert('Sign In button clicked.');
-    }
-  };
+  return (
+    <div>
+      <h1>Welcome to our App!</h1>
+      <p>Please sign in to continue.</p>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSignIn}>Sign In</button>
+    </div>
+  );
+
+  function handleSignIn() {
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+
+      },
+      body: 'username=${username}&password=${password}',
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        alert("success");
+      } else {
+        alert("failed");
+      }
+    })
+    .catch((error) => {
+      alert(error);
+    })
+  }
+};
   
-  export default Login;
+export default Login;
   
   
