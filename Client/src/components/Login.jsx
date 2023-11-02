422111e39a46b300be2e25e4915f36f9683a5fef
Erik Ly <erikly11@gmail.com>
2023-11-01 20:48:14 -0500

    Revert "Merge branch 'main' of https://github.com/Perscription-App/ElectronicPrescription"
    This reverts commit b8c40b9cc2f1aad74b26730e46e0376c5d81f963, reversing
    changes made to dd15a0fb33341958b0244b12c82b73ca9bf40c7b.

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,43 +1,34 @@
 import React, { useState } from 'react';
-import './Login.css';
-import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   return (
-    <div className="container">
+    <div>
       <h1>Welcome to our App!</h1>
       <p>Please sign in to continue.</p>
       <input
-      className='input'
         type="text"
-        placeholder="username"
+        placeholder="Username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
       <input
-      className='input'
         type="password"
-        placeholder="password"
+        placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <span>
       <button onClick={handleSignIn}>Sign In</button>
-      <Link to="/register">or click here to register.</Link>
-      </span>
     </div>
   );
 
   function handleSignIn() {
     fetch('/login', {
       method: 'POST',
-      body: {
-        username: username,
-        password: password
-      }
+      
+      body: 'username=${username}&password=${password}',
     })
     .then((response) => {
       if (response.status === 200) {
