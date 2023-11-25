891de871826a9d573dd02a4ae326b58737da9379
Erik Ly <erikly11@gmail.com>
2023-11-24 22:59:59 -0600

    api service

--- Client/src/api/apiService.js
+++ Client/src/api/apiService.js
@@ -3,8 +3,13 @@ import axios from 'axios';
 const API_BASE_URL = '';
 
 const apiService = {
-    getAllMedication: () => {
-        return axios.get(`${API_BASE_URL}/medication`);
+    getAllMedication: async () => {
+        try {
+            const response = await axios.get(`${API_BASE_URL}/api/medication`);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
     },
     createData: (newData) => {
         return axios.post(`${API_BASE_URL}/data`, newData);
